1ea4ef7544bbe46738ae8b40a3ca6976f0d3cc5c
Antonio Quental <antonio.quental@gmail.com>
2024-11-04 18:47:13 -0300

    First version

--- gen_keys.py
+++ gen_keys.py
@@ -1,22 +1,25 @@
-from stellar_base import Keypair
+from stellar_sdk import Keypair
+
 
 def generate_keypair():
-  """
-  Generates a random keypair and stores it in a secret file.
-  """
-  # Generate a random keypair
-  keypair = Keypair.random()
+    """
+    Generates a random keypair and stores it in a secret file.
+    """
+    # Generate a random keypair
+    keypair = Keypair.random()
 
-  # Extract private and public keys
-  private_key = keypair.secret().decode()
-  public_key = keypair.public_key().decode()
+    # Extract private and public keys
+    private_key = keypair.secret
+    public_key = keypair.public_key
+    print("Public Key: " + keypair.public_key)
+    print("Secret Seed: " + keypair.secret)
 
-  # Write keys to a file (NOT recommended for production)
-  with open("keys.secret", "w") as f:
-    f.write(f"private_key: {private_key}\n")
-    f.write(f"public_key: {public_key}")
+    # Write keys to a file
+    with open("keys.secret", "w") as f:
+        f.write(f"private_key: {private_key}\n")
+        f.write(f"public_key: {public_key}")
 
-  print("Keys generated and stored in keys.secret")
+    print("Keys generated and stored in keys.secret")
 
-if __name__ == "__main__":
-  generate_keypair()
+
+generate_keypair()
